78c540431ad6105d633c00e50422c9587e6296c3
Polina R <pevolkova@gmail.com>
2026-09-21 15:07:14 +0300

    Homework 05

--- src/main/kotlin/lessons/lesson04/homework/Datatypes.kt
+++ src/main/kotlin/lessons/lesson04/homework/Datatypes.kt
@@ -11,8 +11,7 @@ val v9: Long = 4294967296L
 val v10: Float = 18.0f
 val v11: Double = -0.001
 val v12: String = "OpenAI"
-//не должно быть кавычек, только если не имелась в виду строка с выводом слова true
-// val v13: Boolean = "true"
+val v13: String = "true"
 val v14: List <Int> = listOf(3, 14)
 val v15: Char = '9'
 val v16: Int = 2048
@@ -35,16 +34,14 @@ val v29: String = "SpaceX"
 val v31: Double = 0.007
 //кавычки должны быть прямые
 //val v32: String = “🤯”
-//у true и false не должно быть кавычек
-// val v33: Map (Boolean, Short) = mapOf("true" to 2, "false" to 34)
+val v33: Map <String, Short> = mapOf("true" to 2, "false" to 34)
 //не должно быть кавычек
 // val v34: Int = ‘65535’
 val v35: Long = 72057594037927935L
 val v36: Float = 2.71828f
 val v37: Double = 101.0101
 val v38: String = "Quantum Computing"
-//у true и false не должно быть кавычек
-// val v39: Map (Int, Boolean) = mapOf(2 to "true", 34 to "false")
+val v39: Map <Int, String> = mapOf(2 to "true", 34 to "false")
 val v40: Char = 'x'
 val v41: Short = 314
 val v42: Long = 123456789123456789L
@@ -70,7 +67,7 @@ val v43: Float = 6.626f
 // 8. Хранить количество попыток пересчитать звёзды на небе за всю историю человечества.
 //Long
 // 9. Хранить массу воздуха в спускающем матрасе после нападения кота в долях грамма.
-//Double
+//Float
 // 10. Хранить словарь «название стартапа → сумму потерь инвесторов».
 //Map<String, Double>
 // 11. Хранить строку «починилось само», чтобы закрывать тикеты без лишних слов.
